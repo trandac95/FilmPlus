5d4307f3b8f59252c055495e6796b8363b803bf1
Tran Dac <trandc95@gmail.com>
2016-12-07 10:41:48 +0700

    Update

---
+++ app/src/main/java/dph/com/filmplus/Adapter/VideoViewHolder.java
@@ -0,0 +1,26 @@
+package dph.com.filmplus.Adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Tran Dac on 07/12/2016.
+ */
+
+public class VideoViewHolder extends RecyclerView.ViewHolder{
+
+    public final VideoPlayerView mPlayer;
+    public final TextView mTitle;
+    public final ImageView mCover;
+    public final TextView mVisibilityPercents;
+
+    public VideoViewHolder(View view) {
+        super(view);
+        mPlayer = (VideoPlayerView) view.findViewById(R.id.player);
+        mTitle = (TextView) view.findViewById(R.id.title);
+        mCover = (ImageView) view.findViewById(R.id.cover);
+        mVisibilityPercents = (TextView) view.findViewById(R.id.visibility_percents);
+    }
+}
